9e7ac110bb42551e318654f9d4b03e1e0c3c6955
Jaden Fu <jadenjjfu@gmail.com>
2024-08-29 12:47:04 -0400

    Created function to convert from base ten to another base and added inputs to include user interaction.

--- conversion.py
+++ conversion.py
@@ -4,7 +4,9 @@ def int_to_list(number):
     for digit in number:
         numberlist.append(int(digit))
     return numberlist
-def base_converter(number, base, new_base):
+
+
+def convert_to_baseten(number, base):
     numberlist = int_to_list(number)
     baseten = 0
     length = len(numberlist)
@@ -13,5 +15,27 @@ def base_converter(number, base, new_base):
         power = length - opposite
         baseten += digit * base**power
         opposite += 1
-    print(baseten)
-base_converter(123,4,19)
+    return baseten
+
+    
+def convertbase(number,base,newbase):
+    output = ""
+    baseten = int(convert_to_baseten(number, base))
+    output_list = []
+    quotient = baseten
+    while quotient > 0:
+        digit = quotient%newbase
+        output_list.insert(0,int(digit))
+        withoutdigit = quotient-digit
+        quotient = withoutdigit/newbase
+    for digit in output_list:
+        output += str(digit)
+    output = int(output)
+    return output
+try:
+    number = int(input("What is the current number: "))
+    base = int(input("What base is this in: "))
+    newbase = int(input("What base would you like to convert this into: "))
+except ValueError:
+    print("That is not an integer")
+print(convertbase(number,base,newbase))
